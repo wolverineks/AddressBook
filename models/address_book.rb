2e46cfe5bf4c75953741509d74957da4cc2192c0
wolverine.ks <wolverine.ks@gmail.com>
2015-07-05 06:35:42 -0700

    added search

--- models/address_book.rb
+++ models/address_book.rb
@@ -26,13 +26,47 @@ class AddressBook
   end
 
   def import_from_csv(file_name)
-     csv_text = File.read(file_name)
-     csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
- # #8
-     csv.each do |row|
-       row_hash = row.to_hash
-       add_entry(row_hash["name"], row_hash["phone"], row_hash["email"])
+    csv_text = File.read(file_name)
+    csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+
+    csv.each do |row|
+      row_hash = row.to_hash
+      add_entry(row_hash["name"], row_hash["phone"], row_hash["email"])
+    end
+  end
+
+# Search AddressBook.entries for a specific entry by name
+  def binary_search(name)
+
+    lower = 0
+    upper = entries.length - 1
+
+    while lower <= upper
+
+      mid = (lower + upper) / 2
+      mid_name = entries[mid].name
+ 
+      if name == mid_name
+        return entries[mid]
+      elsif name < mid_name
+        upper = mid - 1
+      elsif name > mid_name
+        lower = mid + 1
+      end
     end
 
+    return nil
   end
+
+  def iterative_search(name)
+    result = nil
+    @entries.each do |entry|
+      if entry.name == name
+        result = entry
+        break
+      end
+    end
+    result
+  end
+
 end
